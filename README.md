7bdc39222c0c3ec2453707537fc28d262e84797f
Jozsef Ottucsak <ottucsakjozsef@gmail.com>
2020-11-03 22:26:19 +0100

    Closes #21 - Finalize project

---
+++ README.md
@@ -0,0 +1,31 @@
+# TODO app by Jozsef Ottucsak
+
+## Requirements
+
+* Python 3 (tested on Python 3.8.6 64-bit)
+* Tested on Windows 10 (should work on all OSs)
+* Only Python 3 built-in modules were used
+
+## Installation
+
+Clone or download the zip file to get started. If you have multiple python versions installed replace the python command with the executable name that you usually use to execute Python 3 code.
+
+```
+git clone https://github.com/green-fox-academy/fuzboxz-todo-app.git
+cd fuzboxz-todo-app
+python app.py
+```
+
+## Usage
+
+TODO app is a command-line interface application which uses command line switches to achieve it's functionality. The following functions are currently supported. 
+
+- Start the application without any command line switch to show help
+- **-l** to list the tasks
+- **-a *"My Brand New Task"*** to add a new task
+- **-r *task-id*** to remove a task
+- **-c *task-id*** to complete a task
+
+A *tasklist.csv* was provided as the default "database". It is a plaintext file that uses semicolons to separate columns. The first column is the ID of the given task, the second is the status of the task (if it contains any values that means it's completed) and the third column is description of the task.
+
+If you want to restart from a clean state, delete the **tasklist.csv** file and create a new empty one. The file **must** reside in the same directory as the *app.py* executable. A different name can also be used, in this case the *FILENAME* constant of the *app.py* file must be modified.
--- todo/todo.py
+++ todo/todo.py
@@ -1,39 +1,7 @@
-import sys
-import os
+from todo.io import csvToDictionary, dictionaryToCSV
 
-FILENAME = "tasklist.csv"
-
-
-# Import text to dictionary
-def csvToDictionary(fp):
-    tasks = {}
-    raw = fp.readlines()
-
-    for line in raw:
-        fields = line.split(";")
-        tasks[fields[0]] = {'checked': fields[1], 'desc': fields[2].replace("\n", "")}
-
-    return tasks
-
-
-# Export dictionary to CSV
-def dictionaryToCSV(fp, tasks):
-    # Return to beginning of the file
-    fp.truncate(0)
-    fp.seek(0)
-    for i in tasks:
-        checked = "True" if tasks[i]['checked'] else ""
-        fp.write("{0};{1};{2}\n".format(i, checked, tasks[i]['desc']))
-
-
-# Check if db exists
-def dbExists():
-    directory = os.path.dirname(os.path.realpath(__file__))
-    dbPath = os.sep.join([directory, FILENAME])
-    if (os.path.isfile(dbPath)):
-        return dbPath
-    else:
-        return False
+# Contains all the task related functionality for the todo app
+# Helper functions for file I/O are in the io namespace
 
 
 # List all the tasks
@@ -67,7 +35,7 @@ def addTask(**kwargs):
 
     # Indexing in the CSV starts at 1
     id = len(tasks) + 1
-    tasks[id] = {"checked": False, "desc": args[0]}
+    tasks[id] = {"checked": False, "desc": args[0].replace("\n", "")}
 
     dictionaryToCSV(db, tasks)
     print("New task added: {0} - {1}".format(id, tasks[id]["desc"]))
@@ -125,50 +93,3 @@ def checkTask(**kwargs):
     tasks[i]['checked'] = True
     dictionaryToCSV(db, tasks)
     print("Task checked: {0} - {1}".format(args[0], tasks[i]['desc']))
-
-
-# Displays the command line information when no argument is specified
-def showHelp():
-    title = "Command Line Todo application"
-    decor = "=" * len(title)
-
-    # Construct argument list
-    args = []
-    for arg in functions.keys():
-        args.append("\t{0}\t{1}".format(arg, str(functions[arg][0])))
-    argstring = "\n".join(args)
-
-    # Create final output string
-    print("\n".join(["\n", title, decor, "\n", argstring]))
-
-
-functions = {
-    "-l": ["Lists all the tasks", listTasks],
-    "-a": ["Adds a new task", addTask],
-    "-r": ["Removes a task", removeTask],
-    "-c": ["Completes a task", checkTask]
-}
-
-if __name__ == "__main__":
-    # no arguments
-    if len(sys.argv) == 1:
-        showHelp()
-        exit(0)
-    else:
-        option = sys.argv[1]
-        if (dbPath := dbExists()):
-            if (option in functions.keys()):
-                try:
-                    with open(dbPath, "r+") as db:
-                        functions[option][1](db=db, args=sys.argv[2:])
-                    exit(0)
-                except IOError:
-                    pass
-            else:
-                print("Unsupported argument")
-                showHelp()
-                exit(-1)
-
-        else:
-            print("Error: tasklist.csv not accessible")
-            exit(-1)
